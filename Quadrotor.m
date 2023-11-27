de21930c9f848da7c33c2464269770ebd85eebd9
Ryan Warner <ryan.warner@gatech.edu>
2023-11-26 23:17:43 -0500

    Added Euler transformation

--- Quadrotor.m
+++ Quadrotor.m
@@ -7,7 +7,7 @@ classdef Quadrotor
         thrust (1,4) = [0, 0, 0, 0]                                 % N?
         rotationMatrix (3, 3) = [0, 0, 0; 0, 0, 0; 0, 0, 0]         % Body to inertial
         mass = 0.5                                                  % kg
-        inertiaMatrix (3, 3) = diag([0.0032, 0.0032, 0.0055])         % kgm^2
+        inertiaMatrix (3, 3) = diag([0.0032, 0.0032, 0.0055])       % kgm^2
         rotorTorqueConstant = 0.01691                               % 1/m
         rotorMomentArm = 0.17                                       % m
         gravity = 9.81                                              % m/s^2
@@ -46,6 +46,18 @@ classdef Quadrotor
             obj.rotationMatrix = [cos(theta) * cos(psi), cos(theta) * sin(psi), -sin(theta); ...
                 sin(theta) * sin(phi) * cos(psi) - cos(phi) * sin(psi), sin(theta) * sin(phi) * sin(psi) + cos(phi) * cos(psi), sin(phi) * cos(theta); ...
                 sin(phi) * sin(psi) + cos(phi) * sin(theta) * cos(psi), sin(theta) * sin(psi) * cos(phi) - sin(phi) * sin(psi), cos(theta) * cos(phi)];
+        end
+
+        function accelerations = calcEulerRates(obj)
+            tempBodyRates = num2cell(obj.bodyRates);
+            [p, q, r] = tempBodyRates{:};
+
+            tempEulerRates = num2cell(obj.eulerRates);
+            [phi, theta, ~] = tempEulerRates{:};
+
+            accelerations = [1, tan(theta) * sin(phi), tan(theta) * cos(phi);...
+                0, cos(phi), -sin(phi);...
+                0, sin(phi) / cos(theta), cos(phi) / cos(theta)] * [p; q; r];
         end
 
     end
