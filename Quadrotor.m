d02b94293243b4a52b7a8dc1290def535f46d94d
Ryan Warner <ryan.warner@gatech.edu>
2023-11-26 21:52:25 -0500

    Script setup wrt project tasks

---
+++ Quadrotor.m
@@ -0,0 +1,12 @@
+classdef Quadrotor
+    % Instance variables, should probably do some property validation
+    % Did not know that was a thing ngl
+    properties
+        
+    end
+    
+    % Methods (instance and static)
+    methods
+
+    end
+end
