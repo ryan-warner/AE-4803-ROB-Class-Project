824d451cea4720c570c0da1f65b9ae0d877f05a7
Ryan Warner <ryan.warner@gatech.edu>
2023-11-27 10:17:31 -0500

    Updated with initial conditions and simulation parameters

--- main.m
+++ main.m
@@ -1,6 +1,14 @@
 %% Initialization
 clear all, close all, clc
 
+pos_init = [-3 -2 -1];
+pos_final = [5 3 2];
+
+tf = 8;
+time_step = 0.01;
+
+horizon = tf / time_step;
+
 %% Quadrotor DDP
 
 %% Quadcopter Recursive Model Predictive Control (rMPC)
