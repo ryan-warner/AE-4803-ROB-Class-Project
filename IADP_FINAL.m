3bfc0ce67c81b18941f1d93fb4ed7c3edcda48a8
Ryan Warner <ryan.warner@gatech.edu>
2023-12-13 20:34:40 -0500

    Add line search and visuzalization

--- IADP_FINAL.m
+++ IADP_FINAL.m
@@ -1,16 +1,37 @@
 function [currentGains, iterationCosts] = IADP_FINAL(initial, dynamics, costs, derivatives, options)
     iterationCosts = zeros([1, options.maxIterations]);
     hover_thrust = 0.5 * 9.81 / 4;
-    currentGains = repmat(struct('K', ones(options.m, options.n), 'k', ones(options.m, 1), 'optimal_control', hover_thrust * ones(options.m, 1)), options.horizon, 1);
+    currentGains = repmat(struct('K', randn(options.m, options.n), 'k', randn(options.m, 1), 'optimal_control', hover_thrust * ones(options.m, 1)), options.horizon, 1);
 
     for i = 1:options.maxIterations
-        [iterationCosts(i), currentGains] = step(initial, dynamics, costs, currentGains, derivatives, options);
-        
-        fprintf('Iteration: %i | Cost: %.02f\n', i, iterationCosts(i));
+        [iterationCosts(i), tempGains] = step(initial, dynamics, costs, currentGains, derivatives, options);
+        noiseLevel = 100;        
+        if i > 1 && iterationCosts(i) > iterationCosts(i - 1)
+            fprintf('Cost increased, taking last iteration\n');
+            iterationCosts(i) = iterationCosts(i - 1);
+            currentGains = addNoiseToGains(tempGains, noiseLevel);
+
+        elseif iterationCosts(i) == 0 && i > 1
+            fprintf('Cost is 0, taking last iteration\n');
+            iterationCosts(i) = iterationCosts(i - 1);
+            currentGains = addNoiseToGains(tempGains, noiseLevel);
+        else
+            currentGains = tempGains;
+        end
+
+        fprintf('Iteration: %i | Cost: %.08f\n', i, iterationCosts(i));
     end
 
     function [cost, gains, states] = step(initial, dynamics, costs, currentGains, derivatives, options)
         [states, inputs, cost] = forwardPass(initial, dynamics, costs, currentGains, options);
         gains = backwardPass(states, inputs, derivatives, options);
     end    
+end
+
+function gains = addNoiseToGains(gains, noiseLevel)
+    for j = 1:length(gains)
+        gains(j).K = gains(j).K + noiseLevel * randn(size(gains(j).K));
+        gains(j).k = gains(j).k + noiseLevel * randn(size(gains(j).k));
+        gains(j).optimal_control = gains(j).optimal_control + noiseLevel * randn(size(gains(j).optimal_control));
+    end
 end
--- forwardPass.m
+++ forwardPass.m
@@ -1,18 +1,52 @@
+% function [states, inputs, trajectoryCostsTotal] = forwardPass(initial, dynamics, costs, gains, options)
+%     states = [initial zeros(options.n, options.horizon)];
+%     inputs = zeros(options.m, options.horizon);
+%     trajectoryCosts = zeros(1, options.horizon + 1);
+% 
+%     for i = 1:options.horizon
+%         iterGains = gains(i);
+%         inputs(:, i) =  max(iterGains.K * states(:, i) + iterGains.optimal_control, [0;0;0;0]);
+%         trajectoryCosts(i) = costs.cost(states(:, i), inputs(:, i));
+%         states(:, i + 1) = dynamics(states(:, i), inputs(:, i)) * options.timestep + states(:, i);
+%     end
+% 
+%     trajectoryCosts(end) = costs.finalCost(states(:, end));
+%     trajectoryCostsTotal = sum(trajectoryCosts);
+%     if isnan(trajectoryCostsTotal)
+%         disp('Final cost is NaN');
+%     end
+% end
+
+% Attempt at adding line search
 function [states, inputs, trajectoryCostsTotal] = forwardPass(initial, dynamics, costs, gains, options)
-    states = [initial zeros(options.n, options.horizon)];
-    inputs = zeros(options.m, options.horizon);
-    trajectoryCosts = zeros(1, options.horizon + 1);
+    stepSizes = [1, 0.5, 0.25, 0.1, 0.05, 0.01, 0.001, 0.0001];
+    bestCost = inf;
+    states = [initial zeros(12, 800)];
+    inputs = zeros(4, 800);
+    trajectoryCosts = zeros(1, 800 + 1);
 
-    for i = 1:options.horizon
-        iterGains = gains(i);
-        inputs(:, i) =  max(iterGains.K * states(:, i) + iterGains.optimal_control, [0;0;0;0]);
-        trajectoryCosts(i) = costs.cost(states(:, i), inputs(:, i));
-        states(:, i + 1) = dynamics(states(:, i), inputs(:, i)) * options.timestep + states(:, i);
-    end
+    for stepSize = stepSizes
+        tempStates = [initial zeros(12, 800)];
+        tempInputs = zeros(4, 800);
+        tempCosts = zeros(1, 800 + 1);
 
-    trajectoryCosts(end) = costs.finalCost(states(:, end));
-    trajectoryCostsTotal = sum(trajectoryCosts);
-    if isnan(trajectoryCostsTotal)
-        disp('Final cost is NaN');
+        for i = 1:800
+            iterGains = gains(i);
+            tempInputs(:, i) = iterGains.K * tempStates(:, i) + stepSize * iterGains.k + iterGains.optimal_control;
+            tempCosts(i) = costs.cost(tempStates(:, i), tempInputs(:, i));
+            tempStates(:, i + 1) = dynamics(tempStates(:, i), tempInputs(:, i));
+        end
+
+        tempCosts(end) = costs.finalCost(tempStates(:, end));
+        totalCost = sum(tempCosts);
+
+        if totalCost < bestCost
+            bestCost = totalCost;
+            states = tempStates;
+            inputs = tempInputs;
+            trajectoryCosts = tempCosts;
+        end
+
+        trajectoryCostsTotal = sum(trajectoryCosts);
     end
 end
